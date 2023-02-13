a97583d1dc4866e4e795616232315bda2f75608e
lemezohaib <zhbkhan644@gmail.com>
2023-02-13 19:20:53 +0000

    sm6150-common: Welcome to AwakenOS

--- sm6150.mk
+++ sm6150.mk
@@ -277,7 +277,7 @@ PRODUCT_PACKAGES += \
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-aosp
+    $(LOCAL_PATH)/overlay-awaken
 
 PRODUCT_ENFORCE_RRO_TARGETS += *
 
