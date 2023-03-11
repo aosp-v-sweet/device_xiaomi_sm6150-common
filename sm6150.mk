9e53c22b85788aa700f323195d4de6491e5a7fba
lemezohaib <zhbkhan644@gmail.com>
2023-03-11 15:39:02 +0000

    sm6150-common: Welcome to bootleggers

--- sm6150.mk
+++ sm6150.mk
@@ -281,7 +281,7 @@ PRODUCT_PACKAGES += \
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-aosp
+    $(LOCAL_PATH)/overlay-bootleg
 
 PRODUCT_ENFORCE_RRO_TARGETS += *
 
