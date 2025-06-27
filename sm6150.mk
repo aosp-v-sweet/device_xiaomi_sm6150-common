3bfd27fcc5c1d214d0e75a33d422583c53c7e129
imranpopz <imranpopz32@gmail.com>
2025-06-27 09:34:37 +0000

    sm6150-common : Drop livedisplay

--- sm6150.mk
+++ sm6150.mk
@@ -228,10 +228,6 @@ $(call soong_config_set,lineage_health,charging_control_charging_enabled,0)
 $(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/power_supply/battery/input_suspend)
 $(call soong_config_set,lineage_health,charging_control_supports_bypass,false)
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.1-service.xiaomi_sm6150
-
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
